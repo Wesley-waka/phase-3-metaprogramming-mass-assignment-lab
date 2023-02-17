ad1aed73b50b4bee31eaad046d13c22db7e7f983
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-02-17 15:47:21 +0300

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+
+
+  def initialize(attribute)
+    attribute.each do |key,value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=",value)
+    end
+
+  end
   # your code here
 end
+
+
+susan_attributes = { name: "Susan", height: "5'11\"", eye_color: "Green" }
+
+susan = Person.new(susan_attributes)
+susan.name      # => "Susan"
+susan.height    # => "5'11""
+susan.eye_color # => "Green"
